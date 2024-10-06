bfabc7e680485dafd9a8599fae9e96da84e0019e
George Pylarinos <pilarinos99@yahoo.gr>
2024-10-06 20:07:07 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("icon").addEventListener("click" , function(){
+    document.getElementById("card").style.display = "flex";
+    document.getElementById("icon").style.display = "none";
+})
